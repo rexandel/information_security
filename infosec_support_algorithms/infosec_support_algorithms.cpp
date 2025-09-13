4f6ff5f72dc6f55c435969788258333a1e6246e1
rexandel <xarendel@gmail.com>
2025-09-13 22:00:57 +0300

    Task 1.1.1 (Added getAlphabet function)

---
+++ infosec_support_algorithms/infosec_support_algorithms.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+
+static std::string getAlphabet() {
+    std::string alphabet = "";
+    
+    int startBigChar = 65;
+    int endBigChar = 90;
+
+    for (int character = startBigChar; character < endBigChar; character++) {
+        alphabet += (char)character;
+    }
+
+    int startSmallChar = 97;
+    int endSmallChar = 122;
+
+    for (int character = startSmallChar; character < endSmallChar; character++) {
+        alphabet += (char)character;
+    }
+
+    return alphabet;
+}
+
+int main()
+{
+    std::cout << getAlphabet();
+    return 0;
+}
