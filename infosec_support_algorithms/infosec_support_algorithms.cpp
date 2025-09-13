4cdcb7c65a95f1676497f99062ff4a0854c6bc8a
rexandel <xarendel@gmail.com>
2025-09-13 22:29:49 +0300

    Task 1.1.3 (Alphabet extraction function has been renamed & other minor updates)

--- infosec_support_algorithms/infosec_support_algorithms.cpp
+++ infosec_support_algorithms/infosec_support_algorithms.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <string>
 
-static std::string getAlphabet(int k) {
+static std::string get_alphabet(int k) {
     std::string alphabet = "";
 
     for (char c = 'A'; c <= 'Z' && k > 0; c++, k--) {
@@ -17,7 +17,35 @@ static std::string getAlphabet(int k) {
 
 int main()
 {
-    std::string alphabet = getAlphabet(27);
-    std::cout << alphabet;
+    int count_of_characters_in_alphabet;
+    std::cout << "Enter count of characters in alphabet: ";
+    std::cin >> count_of_characters_in_alphabet;
+
+    if (count_of_characters_in_alphabet > 52 || count_of_characters_in_alphabet < 1) {
+        std::cout << "Count of characters in alphabet is incorrect" << std::endl;
+        return 0;
+    }
+
+    std::string alphabet = get_alphabet(count_of_characters_in_alphabet);
+    std::cout << "Current alphabet: " << alphabet << std::endl;
+
+    std::string encryption_string;
+    std::cout << "Enter encryption string: ";
+    std::cin >> encryption_string;
+
+    if (encryption_string.size() > pow(10, 6) || encryption_string.size() < 1) {
+        std::cout << "Incorrect count of characters in encryption string" << std::endl;
+        return 0;
+    }
+    else {
+        for (auto c : encryption_string) {
+            if (alphabet.find(c) == std::string::npos) {
+                std::cout << "Encryption string contains characters that are not in current alphabet" << std::endl;
+                return 0;
+            }
+        }
+    }
+
+
     return 0;
 }
